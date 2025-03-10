ae44f27aaf185f42084ea123db1faf3b25316199
neeru0713 <neerurani1307@gmail.com>
2025-03-10 20:26:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+    <h1 className='text-red-200'>hello</h1>
+    </div>
+  )
+}
+
+export default App
